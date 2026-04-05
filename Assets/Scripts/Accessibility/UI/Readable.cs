87c976d544fecb99051c15265540b33d4f4f9f68
jess-klost <akloste1@gmail.com>
2026-04-05 15:59:09 -0500

    Created parent class for all readable UI

---
+++ Assets/Scripts/Accessibility/UI/Readable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Readable : MonoBehaviour
+{
+    protected void SpeakUI(string label)
+    {
+        Debug.Log("TTS: " + label);
+        UAP_CustomTTS.Speak(label, 1f);
+    }
+}
