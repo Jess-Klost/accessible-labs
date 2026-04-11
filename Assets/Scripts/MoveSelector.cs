84d9c1aca7c18b31d9e33570136655f592ad48d3
jess-klost <akloste1@gmail.com>
2026-04-10 23:28:44 -0500

    Simplified movement selection

--- Assets/Scripts/MoveSelector.cs
+++ Assets/Scripts/MoveSelector.cs
@@ -1,20 +1,11 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 
 public class MoveSelector : MonoBehaviour
 {
-    public Button moveButton;
     public TMP_Dropdown locationDropdown;
     public MovableObject movableObject;
-
-    /// <summary>
-    /// Keeps track of the current event that is called when moveButton is pressed 
-    /// to move movableObject. This allows the old listener to be removed without
-    /// removing other listeners. 
-    /// </summary>
-    private UnityEngine.Events.UnityAction currentEvent = null;
 
     public void PopulateDropdown(List<MoveLocation> moveLocations)
     {
@@ -25,15 +16,11 @@ public class MoveSelector : MonoBehaviour
         }
         locationDropdown.ClearOptions();
         locationDropdown.AddOptions(names);
-        currentEvent = () => movableObject.MoveTo(0);
-        moveButton.onClick.AddListener(currentEvent);
         locationDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
     }
 
     void OnDropdownValueChanged(int index)
     {
-        moveButton.onClick.RemoveListener(currentEvent);
-        currentEvent = () => movableObject.MoveTo(index);
-        moveButton.onClick.AddListener(currentEvent);
+        movableObject.MoveTo(index);
     }
 }
